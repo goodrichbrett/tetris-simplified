93407b4189fa78984ffa65c222f2d433707115ed
goodrichbrett <brettgoodrich@outlook.com>
2020-08-06 17:20:06 -0500

    define all possible shapes and store in object

--- js/main.js
+++ js/main.js
@@ -2,13 +2,48 @@
 const gameDiv = document.querySelector("#game-div");
 init();
 const blocksNodeList = document.querySelectorAll(".block");
-const blocks = Array.from(blocksNodeList);
 const userScore = document.querySelector("#status #score");
 const resetBtn = document.querySelector("#resetButton");
 const startBtn = document.querySelector("#startButton");
-const colors = ["#e3b505", "#95190c", "610345", "#107e7d", "#044b7f"];
 
 // Variables
+const blocks = Array.from(blocksNodeList);
+// Possible block colors:
+const colors = ["#e3b505", "#95190c", "610345", "#107e7d", "#044b7f"];
+// Define the shapes of each block with indexes
+
+const blockShapes = {
+	blockL: [
+		[1, 11, 21, 2],
+		[10, 11, 12, 22],
+		[1, 11, 21, 20],
+		[10, 20, 21, 22],
+	],
+	blockI: [
+		[1, 11, 21, 31],
+		[10, 11, 12, 13],
+		[1, 11, 21, 31],
+		[10, 11, 12, 13],
+	],
+	blockSq: [
+		[0, 1, 10, 11],
+		[0, 1, 10, 11],
+		[0, 1, 10, 11],
+		[0, 1, 10, 11],
+	],
+	blockT: [
+		[1, 10, 11, 12],
+		[10, 11, 12, 21],
+		[1, 11, 12, 21],
+		[1, 10, 11, 21],
+	],
+	blockSn: [
+		[1, 10, 11, 20],
+		[0, 10, 11, 21],
+		[1, 2, 10, 11],
+		[0, 1, 11, 12],
+	],
+};
 
 // Event Listeners
 
@@ -23,3 +58,10 @@ function init() {
 		gameDiv.appendChild(newDiv);
 	}
 }
+
+function render() {
+	blockShapes.blockL[3].forEach((idx) => {
+		blocks[4 + idx].classList.add("blockL");
+	});
+}
+render();
