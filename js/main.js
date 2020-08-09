25947fa6c5be61c3dd2eff48405ec7997b744e33
goodrichbrett <brettgoodrich@outlook.com>
2020-08-09 13:50:10 -0500

    render function displays different randomly defined shape on screen each refresh

--- js/main.js
+++ js/main.js
@@ -10,40 +10,42 @@ const startBtn = document.querySelector("#startButton");
 const blocks = Array.from(blocksNodeList);
 // Possible block colors:
 const colors = ["#e3b505", "#95190c", "610345", "#107e7d", "#044b7f"];
-// Define the shapes of each block with indexes
-
-const blockShapes = {
-	blockL: [
+// Define the shapes of each block using indexes
+const blockShapes = [
+	(blockL = [
 		[1, 11, 21, 2],
 		[10, 11, 12, 22],
 		[1, 11, 21, 20],
 		[10, 20, 21, 22],
-	],
-	blockI: [
+	]),
+	(blockI = [
 		[1, 11, 21, 31],
 		[10, 11, 12, 13],
 		[1, 11, 21, 31],
 		[10, 11, 12, 13],
-	],
-	blockSq: [
+	]),
+	(blockSq = [
 		[0, 1, 10, 11],
 		[0, 1, 10, 11],
 		[0, 1, 10, 11],
 		[0, 1, 10, 11],
-	],
-	blockT: [
+	]),
+	(blockT = [
 		[1, 10, 11, 12],
 		[10, 11, 12, 21],
 		[1, 11, 12, 21],
 		[1, 10, 11, 21],
-	],
-	blockSn: [
+	]),
+	(blockSn = [
 		[1, 10, 11, 20],
 		[0, 10, 11, 21],
 		[1, 2, 10, 11],
 		[0, 1, 11, 12],
-	],
-};
+	]),
+];
+
+let shape = blockShapes[Math.floor(Math.random() * blockShapes.length)][0];
+let position = 4;
 
 // Event Listeners
 
@@ -59,9 +61,19 @@ function init() {
 	}
 }
 
+//  render on the page the shape with its specific color via CSS selected class by using if statements and adding class list
 function render() {
-	blockShapes.blockL[3].forEach((idx) => {
-		blocks[4 + idx].classList.add("blockL");
+	shape.forEach((idx) => {
+		if (shape === blockShapes[0][0])
+			blocks[position + idx].classList.add("blockL");
+		if (shape === blockShapes[1][0])
+			blocks[position + idx].classList.add("blockI");
+		if (shape === blockShapes[2][0])
+			blocks[position + idx].classList.add("blockSq");
+		if (shape === blockShapes[3][0])
+			blocks[position + idx].classList.add("blockT");
+		if (shape === blockShapes[4][0])
+			blocks[position + idx].classList.add("blockSn");
 	});
 }
 render();
