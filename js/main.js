596b6edead5ec6e88fa8dba3ff49688db9426b0b
goodrichbrett <brettgoodrich@outlook.com>
2020-08-09 14:31:11 -0500

    add start, reset, and timer functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 // Cached Element References
 const gameDiv = document.querySelector("#game-div");
+// Call initialization function after selecting the gameDiv, so the blocks can be adding to the html before selecting each block
 init();
 const blocksNodeList = document.querySelectorAll(".block");
 const userScore = document.querySelector("#status #score");
 const resetBtn = document.querySelector("#resetButton");
 const startBtn = document.querySelector("#startButton");
+const timer = document.querySelector("#timer");
 
 // Variables
 const blocks = Array.from(blocksNodeList);
-// Possible block colors:
 const colors = ["#e3b505", "#95190c", "610345", "#107e7d", "#044b7f"];
+let timerInterval;
+let min,
+	sec,
+	seconds = 0;
+
 // Define the shapes of each block using indexes
 const blockShapes = [
 	(blockL = [
@@ -48,9 +54,17 @@ let shape = blockShapes[Math.floor(Math.random() * blockShapes.length)][0];
 let position = 4;
 
 // Event Listeners
+startBtn.addEventListener("click", () => startTimer());
+resetBtn.addEventListener("click", () => {
+	clearInterval(timerInterval);
+	seconds = 0;
+	clearInterval(timerInterval);
+	removeBlock();
+	timer.innerText = "0m 0s";
+	return (timerInterval = null);
+});
 
 // Functions
-
 function init() {
 	// Create 200 divs for the playing field under gameDiv
 	for (let i = 0; i < 200; i++) {
@@ -75,5 +89,45 @@ function render() {
 		if (shape === blockShapes[4][0])
 			blocks[position + idx].classList.add("blockSn");
 	});
+	updateTimer();
 }
 render();
+
+function removeBlock() {
+	shape.forEach((idx) => {
+		if (shape === blockShapes[0][0])
+			blocks[position + idx].classList.remove("blockL");
+		if (shape === blockShapes[1][0])
+			blocks[position + idx].classList.remove("blockI");
+		if (shape === blockShapes[2][0])
+			blocks[position + idx].classList.remove("blockSq");
+		if (shape === blockShapes[3][0])
+			blocks[position + idx].classList.remove("blockT");
+		if (shape === blockShapes[4][0])
+			blocks[position + idx].classList.remove("blockSn");
+	});
+}
+
+function startTimer() {
+	clearInterval(timerInterval);
+	timerInterval = setInterval(tick, 1000);
+	updateTimer();
+}
+
+function tick() {
+	seconds++;
+	if (seconds === 0) clearInterval(timerInterval);
+	render();
+}
+
+function updateTimer() {
+	if (sec === 59) {
+		min += 1;
+		sec = 0;
+		timer.innerText = `${min}m ${sec}s`;
+	} else {
+		min = Math.floor(seconds / 60);
+		sec = seconds % 60;
+		timer.innerText = `${min}m ${sec}s`;
+	}
+}
